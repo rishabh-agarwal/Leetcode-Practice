150cf876e33ab4e08e7cc45e68ef16188389555d
rishabh-agarwal <rishaba@g.clemson.edu>
2020-03-03 14:40:56 -0600

    chore: mergeTwoLists question

---
+++ src/mergeTwoLists/README.md
@@ -0,0 +1,12 @@
+# 21. Merge Two Sorted Lists
+
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+````aidl
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+````
+
+![](https://github.com/rishabh-agarwal/Leetcode-Practice/blob/master/images/mergeTwoLists.png)
---
+++ src/mergeTwoLists/Solution.java
@@ -0,0 +1,48 @@
+package mergeTwoLists;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        if(l1==null && l2==null)
+            return null;
+        else if(l1==null)
+            return l2;
+        else if(l2==null)
+            return l1;
+
+        ListNode curr = new ListNode(-1);
+        ListNode strPtr = curr;
+
+        while (l1 != null || l2 != null) {
+
+            if (l1 == null) {
+                curr.val = l2.val;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                curr.val = l1.val;
+                l1 = l1.next;
+            } else if (l1.val <= l2.val) {
+                curr.val = l1.val;
+                l1 = l1.next;
+            } else if (l2.val <= l1.val) {
+                curr.val = l2.val;
+                l2 = l2.next;
+            }
+
+            if(l1 != null || l2 != null){
+                ListNode newNode = new ListNode(-1);
+                curr.next = newNode;
+                curr = curr.next;
+            }
+        }
+        return strPtr;
+    }
+}
